24776152c2fa71d0e8ad704ba9c8a8ec45b7b93a
Jesse W. Collins <jesse.w.collins@gmail.com>
2017-02-12 17:08:17 -0500

    interactiveSVG.html is an svg canvas and forms for updating it. There is a form for appending an svg element, a form for setting the svg to a new text, a form for evaluating javascript (which can alter the svg) and a form for adding CSS to the document. Using these forms results in a history element appending the code to the document below the forms. There is also a simple node.js server in app.js used for development. Last there is a stylesheet. The point of writing this is to have a simple REPL for learning SVG and javascript for writing it. I figure SVG might be useful for plotting data.

---
+++ app.js
@@ -0,0 +1,65 @@
+/* app.js */
+//Source for original version of this file: http://stackoverflow.com/a/4720770/895065
+
+//Delete cache so node reloads pages on each request 
+//ref for Object.keys line below: http://stackoverflow.com/questions/1972242/auto-reload-of-files-in-node-js
+Object.keys(require.cache).forEach(function(key) { delete require.cache[key]; 
+	console.log("Key deleted: "+key)}); 
+
+// Load the built-in 'http' module.
+var http = require('http'),
+    fs = require('fs');   
+
+var port = 8080;   
+
+var fidArray = ['./scratch.html','./interactiveSVG.html','./data.js','./style.css']
+
+http.createServer(function(request, response) {  
+        fidArray.forEach(function(fid) {
+          //console.log(fid+" in fidArray for loop");
+          delete require.cache[require.resolve(fid)];
+          }
+          );
+        var fid=request.url
+        console.log("request.url: "+request.url);
+        //response.writeHeader(200, {"Content-Type": "text/html"});
+        if (request.url=='/scratch.html') {
+          response.writeHeader(200, {"Content-Type": "text/html"});
+          fs.readFile('./scratch.html', function (err, html){
+            if (err) {
+              throw err;
+            }
+            response.write(html);
+            response.end();})
+          }
+        if (request.url=='/interactiveSVG.html') {
+          response.writeHeader(200, {"Content-Type": "text/html"});
+          fs.readFile('./interactiveSVG.html', function (err, html){
+            if (err) {
+              console.log('interactiveSVG error')
+              throw err;
+            }
+            response.write(html);
+            response.end();})
+          }
+        if (request.url=='/data.js') {
+          fs.readFile('./data.js', function (err, html){
+            if (err) {
+              console.log('data.js read error')
+              throw err;
+            }
+            //response.write(html);
+            response.end();})
+          }
+        if (request.url=='/style.css') {
+          //response.writeHeader(200, {"Content-Type": "text/html"});
+          fs.readFile('./style.css', function (err, html){
+            if (err) {
+              throw err;
+            }
+            response.write(html);
+            response.end();})
+          }
+    }).listen(port);
+
+console.log('Listening on http://localhost:' + port);
